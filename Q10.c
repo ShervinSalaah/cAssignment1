a23f5175082c4fb6bcd9bc417a0ea98f068bb648
ShervinSalaah <shervinsalaah@gmail.com>
2026-04-13 22:16:12 +0530

    upload Q10

---
+++ Q10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int rows, n=1;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for(int i = 1; i <=rows; i++){
+        for(int j=1; j<=i; j++){
+            printf("%d ", n);
+            n++;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
